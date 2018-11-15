43fae0a8588b6bf230b8ec9d1ade663078b788c5
Petr Hoffmann <petr.hoffic@gmail.com>
2018-11-15 15:39:25 +0000

    Created the app launcher class.

--- src/main/java/com/tawelib/groupfive/view/Library.java
+++ src/main/java/com/tawelib/groupfive/view/Library.java
@@ -3,7 +3,7 @@ package com.tawelib.groupfive.view;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Dashboard extends Application {
+public class Library extends Application {
 
   public static void main(String[] args) {
     launch(args);
@@ -11,6 +11,6 @@ public class Dashboard extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    System.out.println("Hello User!");
+    primaryStage.show();
   }
 }
