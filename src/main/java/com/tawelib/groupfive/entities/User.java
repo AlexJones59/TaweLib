bd4c6547a159cfe38129cbdd0415168cafc43d49
Shree Desai <shree.desai.2407@gmail.com>
2018-11-16 16:45:26 +0000

    Created Entity classes

---
+++ src/main/java/com/tawelib/groupfive/entities/User.java
@@ -0,0 +1,12 @@
+package com.tawelib.groupfive.entities;
+
+public class User {
+
+  private String username ;
+  private String firstName;
+  private String lastName;
+  private String phoneNumber;
+  private Address address;
+
+
+}
