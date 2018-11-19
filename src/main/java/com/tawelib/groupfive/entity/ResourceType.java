dde987f7b4114e899e5094638f750f6c22258135
Shree Desai <shree.desai.2407@gmail.com>
2018-11-19 22:40:36 +0000

    Finished Resource Class

---
+++ src/main/java/com/tawelib/groupfive/entity/ResourceType.java
@@ -0,0 +1,10 @@
+package com.tawelib.groupfive.entity;
+
+/**
+ * ResourceType is an Enum to differentiate between different types of resources.
+ */
+
+public enum ResourceType {BOOK, DVD, LAPTOP}
+
+
+
