d292a09d410919634dd29dfb79468e7c644fffbb
Shree Desai <shree.desai.2407@gmail.com>
2018-11-19 22:40:36 +0000

    Finished Resource Class

--- src/main/java/com/tawelib/groupfive/entity/Resource.java
+++ src/main/java/com/tawelib/groupfive/entity/Resource.java
@@ -4,9 +4,9 @@ import java.io.Serializable;
 import javafx.scene.image.Image;
 
 /**
- * File name: Resource.java
- *    User class stores personal information for the account holder (either the Customer
- *    or Librarian).
+ * File name: Resource.java User class stores personal information for the account holder (either
+ * the Customer or Librarian).
+ *
  * @author Shree Desai
  * @version 0.2
  */
@@ -16,10 +16,109 @@ public abstract class Resource implements Serializable {
   private String title;
   private int year;
   private Image thumbnailImage;
+  private ResourceType type;
 
-  public Resource(String title, int year, Image thumbnailImage) {
+  /**
+   * Instantiates a new Resource.
+   *
+   * @param title the title
+   * @param year the year
+   * @param thumbnailImage the thumbnail image
+   * @param type the type
+   */
+  public Resource(String title, int year, Image thumbnailImage, ResourceType type) {
     this.title = title;
     this.year = year;
     this.thumbnailImage = thumbnailImage;
+    this.type = type;
+  }
+
+  /**
+   * Gets id.
+   *
+   * @return the id
+   */
+  public String getId() {
+    return id;
+  }
+
+  /**
+   * Gets title.
+   *
+   * @return the title
+   */
+  public String getTitle() {
+    return title;
+  }
+
+  /**
+   * Sets title.
+   *
+   * @param title the title
+   */
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  /**
+   * Gets year.
+   *
+   * @return the year
+   */
+  public int getYear() {
+    return year;
+  }
+
+  /**
+   * Sets year.
+   *
+   * @param year the year
+   */
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  /**
+   * Gets thumbnail image.
+   *
+   * @return the thumbnail image
+   */
+  public Image getThumbnailImage() {
+    return thumbnailImage;
+  }
+
+  /**
+   * Sets thumbnail image.
+   *
+   * @param thumbnailImage the thumbnail image
+   */
+  public void setThumbnailImage(Image thumbnailImage) {
+    this.thumbnailImage = thumbnailImage;
+  }
+
+  /**
+   * Gets type.
+   *
+   * @return the type
+   */
+  public ResourceType getType() {
+    return type;
+  }
+
+  /**
+   * Sets type.
+   *
+   * @param type the type
+   */
+  public void setType(ResourceType type) {
+    this.type = type;
+  }
+
+  /**
+   * Gets number of copies of the specific Resource available.
+   */
+  public int getNumberofCopiesAvailable () {
+    //TODO: Once CopyRepository is made, need to call the method.
+    return 0; //also needs to be changed.
   }
 }
