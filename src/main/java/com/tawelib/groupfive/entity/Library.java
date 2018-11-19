aef9d1af815916c7559922c07d336904fc360d74
hoffic-cz <petr.hoffic@gmail.com>
2018-11-19 22:23:42 +0000

    Added name to the Library entity.

--- src/main/java/com/tawelib/groupfive/entity/Library.java
+++ src/main/java/com/tawelib/groupfive/entity/Library.java
@@ -4,4 +4,35 @@ import java.io.Serializable;
 
 public class Library implements Serializable {
 
+  /**
+   * The name of the library.
+   */
+  private String name;
+
+  /**
+   * Creates a new library with a given name.
+   *
+   * @param name The name of the library.
+   */
+  public Library(String name) {
+    this.name = name;
+  }
+
+  /**
+   * Retrieves the name of the library.
+   *
+   * @return The name of the library.
+   */
+  public String getName() {
+    return name;
+  }
+
+  /**
+   * Sets the name of the library.
+   *
+   * @param name New name of the library.
+   */
+  public void setName(String name) {
+    this.name = name;
+  }
 }
