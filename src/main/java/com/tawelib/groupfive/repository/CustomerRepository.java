8f8e7e8d996543809fbb3b38fa17a42dea1ed21b
themis <themismouyiasis1999@gmail.com>
2018-11-20 17:34:55 +0000

    Create username for librarian and user.

--- src/main/java/com/tawelib/groupfive/repository/CustomerRepository.java
+++ src/main/java/com/tawelib/groupfive/repository/CustomerRepository.java
@@ -2,40 +2,47 @@ package com.tawelib.groupfive.repository;
 
 import com.tawelib.groupfive.entity.Customer;
 import com.tawelib.groupfive.exception.AuthenticationException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerRepository implements UserRepository<Customer> {
 
-  private ArrayList<Customer> customers;
+    private ArrayList<Customer> customers;
 
-  public CustomerRepository() {
-    customers = new ArrayList<>();
-  }
+    private static long userNumber = 0;
 
-  @Override
-  public Customer authenticate(String username) {
-    for (Customer customer : customers) {
-      if (true) { //TODO: check if the username is the one we are looking for.
-        return customer;
-      }
+    private static String userPrefix = "US";
+
+    public CustomerRepository() {
+        customers = new ArrayList<>();
     }
 
-    throw new AuthenticationException();
-  }
+    @Override
+    public Customer authenticate(String username) {
+        for (Customer customer : customers) {
+            if (true) { //TODO: check if the username is the one we are looking for.
+                customers.contains(username);
+                return customer;
+            }
+        }
 
-  @Override
-  public void generateUsername(Customer customer) {
-    //TODO: set the customer's username to a generated one making sure it's unique.
-  }
+        throw new AuthenticationException();
+    }
 
-  @Override
-  public List<Customer> getAll() {
-    return customers;
-  }
+    @Override
+    public void generateUsername(Customer customer) {
+        //TODO: set the customer's username to a generated one making sure it's unique.
+        String userId = String.valueOf(userPrefix + userNumber++);
+    }
 
-  @Override
-  public void add(Customer customer) {
-    customers.add(customer);
-  }
+    @Override
+    public List<Customer> getAll() {
+        return customers;
+    }
+
+    @Override
+    public void add(Customer customer) {
+        customers.add(customer);
+    }
 }
--- src/main/java/com/tawelib/groupfive/repository/LibrarianRepository.java
+++ src/main/java/com/tawelib/groupfive/repository/LibrarianRepository.java
@@ -2,40 +2,47 @@ package com.tawelib.groupfive.repository;
 
 import com.tawelib.groupfive.entity.Librarian;
 import com.tawelib.groupfive.exception.AuthenticationException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LibrarianRepository implements UserRepository<Librarian> {
 
-  private ArrayList<Librarian> librarians;
+    private ArrayList<Librarian> librarians;
 
-  public LibrarianRepository() {
-    librarians = new ArrayList<>();
-  }
+    private static long librarianNumber = 0;
 
-  @Override
-  public Librarian authenticate(String username) {
-    for (Librarian librarian : librarians) {
-      if (true) { //TODO: check if the username is the one we are looking for.
-        return librarian;
-      }
+    private static String LibrarianPrefix = "LB";
+
+    public LibrarianRepository() {
+        librarians = new ArrayList<>();
     }
 
-    throw new AuthenticationException();
-  }
+    @Override
+    public Librarian authenticate(String username) {
+        for (Librarian librarian : librarians) {
+            if (true) { //TODO: check if the username is the one we are looking for.
+                librarians.contains(username);
+                return librarian;
+            }
+        }
 
-  @Override
-  public void generateUsername(Librarian librarian) {
-    //TODO: set the librarian's username to a generated one making sure it's unique.
-  }
+        throw new AuthenticationException();
+    }
 
-  @Override
-  public List<Librarian> getAll() {
-    return librarians;
-  }
+    @Override
+    public void generateUsername(Librarian librarian) {
+        //TODO: set the librarian's username to a generated one making sure it's unique.
+        String librarianId = String.valueOf(LibrarianPrefix + librarianNumber++);
+    }
 
-  @Override
-  public void add(Librarian librarian) {
-    librarians.add(librarian);
-  }
+    @Override
+    public List<Librarian> getAll() {
+        return librarians;
+    }
+
+    @Override
+    public void add(Librarian librarian) {
+        librarians.add(librarian);
+    }
 }
