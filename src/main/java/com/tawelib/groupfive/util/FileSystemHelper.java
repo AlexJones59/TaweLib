b6dbd5d8baa14d9965288ebb0b078d7fcaf9f215
hoffic-cz <petr.hoffic@gmail.com>
2018-11-20 00:36:36 +0000

    Created the FileSystemHelper class.

---
+++ src/main/java/com/tawelib/groupfive/util/FileSystemHelper.java
@@ -0,0 +1,77 @@
+package com.tawelib.groupfive.util;
+
+import com.tawelib.groupfive.entity.Library;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Takes care of all file system related operations.
+ *
+ * @author Petr Hoffmann
+ */
+public class FileSystemHelper {
+
+  private static final String LIBRARY_SAVE_DIR = "data/";
+
+  private FileSystemHelper() {
+    // An empty constructor to prevent instantiating this class.
+  }
+
+  /**
+   * Saves a given library to the file system.
+   *
+   * @param library Library to be saved.
+   * @throws IOException Unable to save the changes.
+   */
+  public static void saveLibraryToFile(Library library) throws IOException {
+    String path = getLibraryPath(library.getName());
+
+    FileOutputStream outputStream = new FileOutputStream(path);
+    ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+    objectOutputStream.writeObject(library);
+
+    objectOutputStream.close();
+  }
+
+  public static Library getLibrary(String name) throws IOException, ClassNotFoundException {
+    Library library;
+
+    try {
+      library = loadLibraryFromFile(getLibraryPath(name));
+    } catch (FileNotFoundException e) {
+      library = new Library(name);
+    }
+
+    return library;
+  }
+
+  /**
+   * Reads a Library from the file system.
+   *
+   * If the file path supplied is null a default path is used.
+   *
+   * @return Library loaded from the file system.
+   */
+  private static Library loadLibraryFromFile(String name)
+      throws IOException, ClassNotFoundException {
+
+    String path = getLibraryPath(name);
+    FileInputStream inputStream = new FileInputStream(path);
+    ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+    try {
+      return (Library) objectInputStream.readObject();
+    } finally {
+      objectInputStream.close();
+    }
+  }
+
+  private static String getLibraryPath(String name) {
+    return LIBRARY_SAVE_DIR + name;
+  }
+}
--- src/main/java/com/tawelib/groupfive/view/Library.java
+++ src/main/java/com/tawelib/groupfive/view/Library.java
@@ -1,9 +1,15 @@
 package com.tawelib.groupfive.view;
 
+import com.tawelib.groupfive.util.FileSystemHelper;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Library extends Application {
+
+  public static final String DEFAULT_LIBRARY_NAME = "Tawe-Lib";
+
+  private com.tawelib.groupfive.entity.Library library;
 
   public static void main(String[] args) {
     launch(args);
@@ -11,6 +17,21 @@ public class Library extends Application {
 
   @Override
   public void start(Stage primaryStage) {
+    try {
+      library = FileSystemHelper.getLibrary(DEFAULT_LIBRARY_NAME);
+    } catch (Exception e) {
+      //TODO: Announce that the library could not be loaded nor created.
+    }
+
     primaryStage.show();
+  }
+
+  @Override
+  public void stop() {
+    try {
+      FileSystemHelper.saveLibraryToFile(library);
+    } catch (IOException e) {
+      //TODO: Announce that the library could not be saved.
+    }
   }
 }
