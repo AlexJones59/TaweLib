75a80076878c7a4ae187da57d1fb78ffa916e4fe
Shree Desai <shree.desai.2407@gmail.com>
2018-11-20 15:08:09 +0000

    Finished Fine and Transaction Class.

--- src/main/java/com/tawelib/groupfive/entity/Fine.java
+++ src/main/java/com/tawelib/groupfive/entity/Fine.java
@@ -1,5 +1,78 @@
 package com.tawelib.groupfive.entity;
 
+import java.util.Date;
+
+/**
+ * File Name - Fine.java
+ * The ‘Fine’ class is the class that stores information regarding a fine,
+ * which is created automatically when a copy is overdue.
+ *
+ * @author Shree Desai
+ * @version 0.2
+ */
 public class Fine {
+
+  private int amount;
+  private Date dateIssued;
+  private Lease specificLease;
+
+  /**
+   * Instantiates a new Fine for resources that overdue.
+   *
+   */
+  public Fine(Lease specificLease) {
+    this.dateIssued = new Date();
+    this.specificLease = specificLease;
+  }
+
+  /**
+   * Instantiates a new Fine, for specific fines that the librarian can define straight away.
+   *
+   * @param amount the amount
+   */
+  public Fine(int amount, Lease specificLease) {
+    this.amount = amount;
+    this.dateIssued = new Date();
+    this.specificLease = specificLease;
+  }
+
+  /**
+   * Gets amount.
+   *
+   * @return the amount
+   */
+  public int getAmount() {
+    return amount;
+  }
+
+  /**
+   * Gets date issued.
+   *
+   * @return the date issued
+   */
+  public Date getDateIssued() {
+    return dateIssued;
+  }
+
+  /**
+   * Gets instance of Lease that this fine is specific to.
+   *
+   */
+  public Lease getSpecificLease() {
+    return specificLease;
+  }
+
+  /**
+   * Gets days overdue.
+   *
+   */
+  public int getDaysOverdue() {
+    //TODO: optimise.
+    Date currentDate = new Date();
+    long diffInMilli = currentDate.getTime() - dateIssued.getTime();
+    return (int)((((diffInMilli / 1000)/60)/60)/24);
+
+  }
+
 
 }
--- src/main/java/com/tawelib/groupfive/entity/Transaction.java
+++ src/main/java/com/tawelib/groupfive/entity/Transaction.java
@@ -1,5 +1,56 @@
 package com.tawelib.groupfive.entity;
 
+import java.util.Date;
+
+/**
+ * File Name - Transaction.java The ‘Transaction’ class is the class that records every time a
+ * customer pays funds to the Library to change their account balance.
+ *
+ * @author Shree Desai
+ * @version 0.2
+ */
 public class Transaction {
 
+  private int amount;
+  private Date datePaid;
+  private Customer payee;
+
+  /**
+   * Instantiates a new Transaction.
+   *
+   * @param amount the amount
+   * @param payee the customer paying
+   */
+  public Transaction(int amount, Customer payee) {
+    this.amount = amount;
+    this.payee = payee;
+    this.datePaid = new Date();
+  }
+
+  /**
+   * Gets amount.
+   *
+   * @return the amount
+   */
+  public int getAmount() {
+    return amount;
+  }
+
+  /**
+   * Gets date paid.
+   *
+   * @return the date paid
+   */
+  public Date getDatePaid() {
+    return datePaid;
+  }
+
+  /**
+   * Gets payee.
+   *
+   * @return the payee
+   */
+  public Customer getPayee() {
+    return payee;
+  }
 }
