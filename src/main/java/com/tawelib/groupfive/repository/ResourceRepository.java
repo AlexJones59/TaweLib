14dbefaad26229827fd75b58e8324257a2961c37
themis <themismouyiasis1999@gmail.com>
2018-11-21 13:14:21 +0000

    Update Resource repository.

--- src/main/java/com/tawelib/groupfive/repository/ResourceRepository.java
+++ src/main/java/com/tawelib/groupfive/repository/ResourceRepository.java
@@ -1,9 +1,36 @@
 package com.tawelib.groupfive.repository;
 
+import com.tawelib.groupfive.entity.Book;
+import com.tawelib.groupfive.entity.Customer;
 import com.tawelib.groupfive.entity.Resource;
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class ResourceRepository implements BaseRepository<Resource> {
+
+  public Resource getById(String id) {
+    return null;
+  }
+
+  public Book getBookByIsbn(String isbn) {
+    return null;
+  }
+
+  private void generateId(Resource resource) {
+
+  }
+
+  public void addRequests(Resource resource, Customer customer) {
+
+  }
+
+  public Resource searchResources(String query, Type types) {
+    return null;
+  }
+
+  public void listResources(Resource after, int limit, Type types) {
+
+  }
 
   @Override
   public List<Resource> getAll() {
