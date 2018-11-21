0578104715459b3002782bda1f5cbc18bf1c92aa
themis <themismouyiasis1999@gmail.com>
2018-11-21 13:30:04 +0000

    Update librarian username.

--- src/main/java/com/tawelib/groupfive/repository/LibrarianRepository.java
+++ src/main/java/com/tawelib/groupfive/repository/LibrarianRepository.java
@@ -38,7 +38,7 @@ public class LibrarianRepository implements UserRepository<Librarian> {
         LIBRARIAN_PREFIX,
         librarianNumber
     );
-    librarianNumber++
+    librarianNumber++;
   }
 
   @Override
