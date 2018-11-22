52ccc6771f07aa21e7f990c77e82132f0ddcf397
Shree Desai <shree.desai.2407@gmail.com>
2018-11-22 15:46:37 +0000

    Finished Copy and Request Classes, Made enums; CopyStatus, RequestStatus. Did minor changes to javadoc and logic in others.

--- src/main/java/com/tawelib/groupfive/entity/ResourceType.java
+++ src/main/java/com/tawelib/groupfive/entity/ResourceType.java
@@ -2,6 +2,9 @@ package com.tawelib.groupfive.entity;
 
 /**
  * ResourceType is an Enum to differentiate between different types of resources.
+ *
+ * @author Shree Desai
+ * @version 0.1
  */
 
 public enum ResourceType { BOOK, DVD, LAPTOP }
