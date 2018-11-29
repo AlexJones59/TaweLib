5397c6cd02046caf51fcc299f389f9960e4e981f
hoffic-cz <petr.hoffic@gmail.com>
2018-11-29 23:25:27 +0000

    Added JavaDoc to the ResourceHelper.

--- src/main/java/com/tawelib/groupfive/util/ResourceHelper.java
+++ src/main/java/com/tawelib/groupfive/util/ResourceHelper.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+/**
+ * Locates the project's resources (assets).
+ *
+ * @author Petr Hoffmann
+ */
 public class ResourceHelper {
 
   private ResourceHelper() {
@@ -13,6 +18,13 @@ public class ResourceHelper {
   public static final String FXML_VIEWS_DIR =
       "/src/main/java/com/tawelib/groupfive/view/";
 
+  /**
+   * Returns the URL of the requested resource.
+   *
+   * @param resourceName The name of the resource.
+   * @return URL of the view.
+   * @throws MalformedURLException When unable to parse the URL.
+   */
   public static URL getViewUrl(String resourceName)
       throws MalformedURLException {
     File file = new File(
