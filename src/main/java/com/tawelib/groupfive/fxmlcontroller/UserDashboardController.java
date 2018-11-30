ec57762d6e7ce457baab5a91485d02b203a23a18
hoffic-cz <petr.hoffic@gmail.com>
2018-11-30 07:33:38 +0000

    User type-specific GUI methods refactoring.

--- src/main/java/com/tawelib/groupfive/fxmlcontroller/UserDashboardController.java
+++ src/main/java/com/tawelib/groupfive/fxmlcontroller/UserDashboardController.java
@@ -4,6 +4,7 @@ import com.tawelib.groupfive.entity.Librarian;
 import com.tawelib.groupfive.util.SceneHelper;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class UserDashboardController extends BaseFxmlController {
@@ -21,7 +22,31 @@ public class UserDashboardController extends BaseFxmlController {
   public TextField addressTextField;
 
   @FXML
+  public Label staffNumberLabel;
+
+  @FXML
   public TextField staffNumberTextField;
+
+  @FXML
+  public Button browseResourceButton;
+
+  @FXML
+  public Button manageAccountButton;
+
+  @FXML
+  public Button transactionsAndFinesButton;
+
+  @FXML
+  public Button createNewAccountButton;
+
+  @FXML
+  public Button overdueCopiesButton;
+
+  @FXML
+  public Button manageUsersButton;
+
+  @FXML
+  public Button manageResourcesButton;
 
   public UserDashboardController() {
   }
@@ -31,19 +56,36 @@ public class UserDashboardController extends BaseFxmlController {
    */
   @Override
   public void refresh() {
+    setGuiForUsers();
+
+    if (loggedInUser.getClass().equals(Librarian.class)) {
+      setGuiForLibrarians();
+    } else {
+      setGuiForCustomers();
+    }
+  }
+
+  private void setGuiForUsers() {
     usernameTextField.setText(loggedInUser.getUsername());
     fullNameTextField.setText(loggedInUser.getFullName());
     //TODO: Format Address nicely.
     addressTextField.setText(loggedInUser.getAddress().toString());
+  }
 
-    if (loggedInUser.getClass().equals(Librarian.class)) {
-      staffNumberTextField.setText(
-          String.format(
-              "%d",
-              ((Librarian) loggedInUser).getStaffNumber()
-          )
-      );
-    }
+  private void setGuiForLibrarians() {
+    staffNumberLabel.setVisible(true);
+    staffNumberTextField.setVisible(true);
+    staffNumberTextField.setText(
+        String.format(
+            "%d",
+            ((Librarian) loggedInUser).getStaffNumber()
+        )
+    );
+  }
+
+  private void setGuiForCustomers() {
+    staffNumberLabel.setVisible(false);
+    staffNumberTextField.setVisible(false);
   }
 
   /**
