3a34fe0c4dfaa2f1fb77b7292a0ed0d3ab56f160
hoffic-cz <petr.hoffic@gmail.com>
2018-12-03 23:33:23 +0000

    Fixed ResourceRepository.

--- src/main/java/com/tawelib/groupfive/repository/ResourceRepository.java
+++ src/main/java/com/tawelib/groupfive/repository/ResourceRepository.java
@@ -18,11 +18,15 @@ import java.util.List;
  */
 public class ResourceRepository implements BaseRepository<Resource> {
 
-  private static ArrayList<Resource> resources;
+  private ArrayList<Resource> resources;
 
   private int lastBookNumber = 0;
   private int lastDvdNumber = 0;
   private int lastLaptopNumber = 0;
+
+  public ResourceRepository() {
+    resources = new ArrayList<>();
+  }
 
   /**
    * Generates unique id as resource is persisted to repository.
@@ -34,23 +38,15 @@ public class ResourceRepository implements BaseRepository<Resource> {
     String newResourceId = "";
 
     //Checks type of Resource and creates Id based upon that...
-    switch (resource.getType()) {
-      case DVD: {
-        typePrefix = "D";
-        newResourceId = typePrefix + Integer.toString(lastDvdNumber);
-        break;
-      }
-      case BOOK: {
-        typePrefix = "B";
-        newResourceId = typePrefix + Integer.toString(lastBookNumber);
-        break;
-      }
-      case LAPTOP: {
-        typePrefix = "L";
-        newResourceId = typePrefix + Integer.toString(lastLaptopNumber);
-        break;
-      } default:
-
+    if (resource.getClass().equals(Dvd.class)) {
+      typePrefix = "D";
+      newResourceId = typePrefix + Integer.toString(lastDvdNumber);
+    } else if (resource.getClass().equals(Book.class)) {
+      typePrefix = "B";
+      newResourceId = typePrefix + Integer.toString(lastBookNumber);
+    } else if (resource.getClass().equals(Laptop.class)) {
+      typePrefix = "L";
+      newResourceId = typePrefix + Integer.toString(lastLaptopNumber);
     }
 
     try {
@@ -61,8 +57,6 @@ public class ResourceRepository implements BaseRepository<Resource> {
     } catch (IllegalAccessException | NoSuchFieldException e) {
       e.printStackTrace();
     }
-
-
   }
 
   /**
@@ -73,8 +67,9 @@ public class ResourceRepository implements BaseRepository<Resource> {
    */
   public List<Book> searchBook(String query) {
     ArrayList<Book> result = new ArrayList<>();
+
     for (Resource searchResource : resources) {
-      if (searchResource.getType() == ResourceType.BOOK) {
+      if (searchResource.getClass().equals(Book.class)) {
         Book searchBook = (Book) searchResource;
         if (searchBook.getTitle().equals(query)
             || searchBook.getYear() == Integer.valueOf(query)
@@ -86,10 +81,7 @@ public class ResourceRepository implements BaseRepository<Resource> {
       }
     }
 
-    if (!result.isEmpty()) {
-      return result;
-    }
-    return null;
+    return result;
 
   }
 
@@ -101,8 +93,9 @@ public class ResourceRepository implements BaseRepository<Resource> {
    */
   public List<Dvd> searchDvd(String query) {
     ArrayList<Dvd> result = new ArrayList<>();
+
     for (Resource searchResource : resources) {
-      if (searchResource.getType() == ResourceType.DVD) {
+      if (searchResource.getClass().equals(Dvd.class)) {
         Dvd searchDvd = (Dvd) searchResource;
         if (searchDvd.getTitle().equals(query)
             || searchDvd.getYear() == Integer.valueOf(query)
@@ -113,10 +106,7 @@ public class ResourceRepository implements BaseRepository<Resource> {
       }
     }
 
-    if (!result.isEmpty()) {
-      return result;
-    }
-    return null;
+    return result;
 
   }
 
@@ -129,8 +119,9 @@ public class ResourceRepository implements BaseRepository<Resource> {
    */
   public List<Laptop> searchLaptop(String query, String searchAttribute) {
     ArrayList<Laptop> result = new ArrayList<>();
+
     for (Resource searchResource : resources) {
-      if (searchResource.getType() == ResourceType.LAPTOP) {
+      if (searchResource.getClass().equals(Laptop.class)) {
         Laptop searchLaptop = (Laptop) searchResource;
         if (searchLaptop.getTitle().equals(query)
             || searchLaptop.getYear() == Integer.valueOf(query)
@@ -142,10 +133,8 @@ public class ResourceRepository implements BaseRepository<Resource> {
         }
       }
     }
-    if (!result.isEmpty()) {
-      return result;
-    }
-    return null;
+
+    return result;
   }
 
   /**
@@ -204,9 +193,8 @@ public class ResourceRepository implements BaseRepository<Resource> {
   @Override
   public void add(Resource resource) {
     if (!resources.contains((resource))) {
+      generateId(resource);
       resources.add(resource);
     }
   }
-
-
 }
