7f140aec09c8c53d0342715169498f0851a7e061
Shree Desai <shree.desai.2407@gmail.com>
2018-12-03 16:36:43 +0000

    Fixed Checkstyle Conflicts

--- src/main/java/com/tawelib/groupfive/fxmlcontroller/FineInformationController.java
+++ src/main/java/com/tawelib/groupfive/fxmlcontroller/FineInformationController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class FineInformationController extends BaseFxmlController{
+public class FineInformationController extends BaseFxmlController {
 
   @FXML
   private Button btnBack;
