3a059f7f6076ea67810a88d5d792c9992145549b
hoffic-cz <petr.hoffic@gmail.com>
2018-12-04 14:25:51 +0000

    Finished adding funds to an account.

--- src/main/java/com/tawelib/groupfive/fxmlcontroller/NewTransactionController.java
+++ src/main/java/com/tawelib/groupfive/fxmlcontroller/NewTransactionController.java
@@ -1,7 +1,10 @@
 package com.tawelib.groupfive.fxmlcontroller;
 
+import com.tawelib.groupfive.entity.Customer;
+import com.tawelib.groupfive.util.AlertHelper;
 import com.tawelib.groupfive.util.SceneHelper;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
@@ -13,16 +16,41 @@ public class NewTransactionController extends BaseFxmlController {
   @FXML
   public TextField amountTextField;
 
-  @Override
-  public void refresh() {
-
-  }
-
   public NewTransactionController() {
   }
 
-  public void topUp() {
+  @Override
+  public void refresh() {
+    usernameLabel.setText(selectedUser.getUsername());
+  }
 
+  /**
+   * Adds entered amount of funds to the user's balance.
+   */
+  public void topUp() {
+    if (selectedUser.getClass().equals(Customer.class)) {
+      Customer selectedCustomer = (Customer) selectedUser;
+
+      try {
+        double fundsBeingAdded = Double.parseDouble(amountTextField.getText());
+
+        int fundsInPennies = (int) (fundsBeingAdded * 100);
+
+        selectedCustomer.increaseAccountBalance(fundsInPennies);
+
+        AlertHelper.alert(
+            AlertType.INFORMATION,
+            "Funds added!"
+        );
+
+        back();
+      } catch (NumberFormatException e) {
+        AlertHelper.alert(
+            AlertType.WARNING,
+            "You must enter a number."
+        );
+      }
+    }
   }
 
   public void back() {
