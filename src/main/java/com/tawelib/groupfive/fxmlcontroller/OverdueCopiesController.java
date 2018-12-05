b44195e2759c2d439d5974cdd4fff0a2c8b97e41
Shree Desai <shree.desai.2407@gmail.com>
2018-12-05 14:10:24 +0000

    Made basic classes needed for Overdue Copies Screen

--- src/main/java/com/tawelib/groupfive/fxmlcontroller/OverdueCopiesController.java
+++ src/main/java/com/tawelib/groupfive/fxmlcontroller/OverdueCopiesController.java
@@ -1,12 +1,58 @@
 package com.tawelib.groupfive.fxmlcontroller;
 
+import com.tawelib.groupfive.entity.Lease;
+import com.tawelib.groupfive.entity.ResourceType;
+import com.tawelib.groupfive.tablewrapper.OverdueCopiesTableWrapper;
 import com.tawelib.groupfive.util.SceneHelper;
+import java.util.Date;
+import java.util.List;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 public class OverdueCopiesController extends BaseFxmlController {
+
+  @FXML
+  private Button btnBack;
+
+  @FXML
+  private Label lblScreenTitle;
+
+  @FXML
+  private TextField txtSearchQuery;
+
+  @FXML
+  private Label lblSearch;
+
+  @FXML
+  private TableView<OverdueCopiesTableWrapper> tblOverdueCopies;
+
+  @FXML
+  private TableColumn<OverdueCopiesTableWrapper, String> usernameColumn;
+
+  @FXML
+  private TableColumn<OverdueCopiesTableWrapper, String> titleColumn;
+
+  @FXML
+  private TableColumn<OverdueCopiesTableWrapper, String> idColumn;
+
+  @FXML
+  private TableColumn<OverdueCopiesTableWrapper, ResourceType> typeColumn;
+
+  @FXML
+  private TableColumn<OverdueCopiesTableWrapper, Date> dateColumn;
+
+  @FXML
+  private TableColumn<OverdueCopiesTableWrapper, String> fineColumn;
 
 
   public OverdueCopiesController() {
   }
+
+
 
   /**
    * Sets the dynamic fields.
---
+++ src/main/java/com/tawelib/groupfive/tablewrapper/OverdueCopiesTableWrapper.java
@@ -0,0 +1,15 @@
+package com.tawelib.groupfive.tablewrapper;
+
+import com.tawelib.groupfive.entity.Lease;
+
+/**
+ * The type Overdue copies table wrapper.
+ */
+public class OverdueCopiesTableWrapper {
+
+  public OverdueCopiesTableWrapper(Lease lease) {
+
+
+  }
+
+}
