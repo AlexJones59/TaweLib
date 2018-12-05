656167920d39ea3e6c619113a30bedce22a2d88b
Nayeem Mohammed <Nayeemmohammed1996@gmail.com>
2018-12-05 15:44:35 +0000

    Created test data. 2 customers, 2 librarians, 2 books, 2 laptops. List is being problematic for DVD's

--- src/main/java/com/tawelib/groupfive/testdata/EntityTestData.java
+++ src/main/java/com/tawelib/groupfive/testdata/EntityTestData.java
@@ -3,12 +3,17 @@ package com.tawelib.groupfive.testdata;
 import com.tawelib.groupfive.entity.Book;
 import com.tawelib.groupfive.entity.Copy;
 import com.tawelib.groupfive.entity.Customer;
+import com.tawelib.groupfive.entity.Dvd;
+import com.tawelib.groupfive.entity.Laptop;
 import com.tawelib.groupfive.entity.Lease;
 import com.tawelib.groupfive.entity.Librarian;
 import com.tawelib.groupfive.entity.Library;
 import com.tawelib.groupfive.repository.CustomerRepository;
 import com.tawelib.groupfive.repository.LibrarianRepository;
+
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class EntityTestData {
 
@@ -26,10 +31,23 @@ public class EntityTestData {
             "System",
             "Admin",
             "Phone #",
-            "110",
+            "1",
             "The street",
             "Swansea",
             "SA28PJ",
+            new Date()
+        )
+    );
+
+    librarianRepository.add(
+        new Librarian(
+            "happy",
+            "Admin",
+            "Phone # :D",
+            "2",
+            "The better street",
+            "Cardiff",
+            "SA1 4LL",
             new Date()
         )
     );
@@ -45,6 +63,18 @@ public class EntityTestData {
         )
     );
 
+    customerRepository.add(
+        new Customer(
+            "Sombrero",
+            "Customer",
+            "pon nombrero",
+            "uno",
+            "Some street",
+            "MEXICOOO",
+            "MX03 2UT"
+        )
+    );
+
     Book book = new Book(
         "The tiTTle",
         2010,
@@ -55,7 +85,61 @@ public class EntityTestData {
         "IZBNN",
         "C#"
     );
+
+    Book book1 = new Book(
+        "The Dunwich Horror",
+        1980,
+        null,
+        "H.P. Lovecraft",
+        "No clue",
+        "Cosmic Horror",
+        "IZBN",
+        "R'lyeh"
+    );
+
+    /*Dvd dvd = new Dvd(
+        "DVDdddD",
+        2018,
+        null,
+        "Director",
+        60,
+        List<String> languages = Arrays.asList ("English", "Spanish"),
+        List<String> sublanguages = Arrays.asList ("English", "Spanish")
+    );
+
+    Dvd dvd1 = new Dvd(
+        "Intersteller",
+        2016,
+        null,
+        "Christopher Nolan",
+        420,
+        List<String> languages = Arrays.asList ("English", "Spanish", "Engrish"),
+        List<String> sublanguages = Arrays.asList ("English", "Engrish")
+    );*/
+
+    Laptop laptop = new Laptop(
+        "New Laptop",
+        2016,
+        null,
+        "Manufaturer",
+        "Model",
+        "installedOperatingSystem"
+    );
+
+    Laptop laptop1 = new Laptop(
+        "HP Laptop",
+        2018,
+        null,
+        "HP",
+        "HP 5000 ?",
+        "Windows (Better than Linux)"
+    );
+
+
     library.getResourceRepository().add(book);
+    library.getResourceRepository().add(book1);
+    library.getResourceRepository().add(laptop);
+    library.getResourceRepository().add(laptop1);
 
     Copy copy = new Copy(book);
     library.getCopyRepository().add(copy);
