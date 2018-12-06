e4d9c19661a6b8f8427e0effa11ab18855cf0fe8
Nayeem Mohammed <Nayeemmohammed1996@gmail.com>
2018-12-06 20:33:50 +0000

    AS

--- src/main/java/com/tawelib/groupfive/fxmlcontroller/BorrowResourcesController.java
+++ src/main/java/com/tawelib/groupfive/fxmlcontroller/BorrowResourcesController.java
@@ -1,23 +1,19 @@
 package com.tawelib.groupfive.fxmlcontroller;
 
-import com.tawelib.groupfive.entity.Book;
 import com.tawelib.groupfive.entity.Customer;
-import com.tawelib.groupfive.entity.Request;
-import com.tawelib.groupfive.entity.User;
 import com.tawelib.groupfive.util.AlertHelper;
 import com.tawelib.groupfive.util.SceneHelper;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 /**
  * Allows Librarians to let loan resources to customers that have above minimum account balance.
- *  @author Nayeem
- *  @version 0.1
+ *
+ * @author Nayeem
+ * @version 0.1
  */
 public class BorrowResourcesController extends BaseFxmlController {
 
@@ -40,17 +36,16 @@ public class BorrowResourcesController extends BaseFxmlController {
 
   /**
    * Checks whether user can borrow a resource and then lets them borrow specified resource.
-   *
    */
   public void borrow() {
     if (selectedUser.getAccountBalance() < 0) {
       AlertHelper.alert(AlertType.WARNING, "Your balance is below the minimum.");
-    } else {
+    } /*else {
       //if (CopyManager.borrowResourceCopy.txtResourceCopyId.getText())
 
-      CopyManager.borrowResourceCopy(library, txtResourceCopyId.getText(),selectedUser);
-    }
+      CopyManager.borrowResourceCopy(library, txtResourceCopyId.getText(), selectedUser);*/
   }
+
 
   public void cancel() {
     SceneHelper.setUpScene(this, "UserInformation");
