3ba5405ee3a256713e43e95be122ba4ff744f945
hoffic-cz <petr.hoffic@gmail.com>
2018-12-07 18:41:47 +0000

    Checked code style and Javadoc for the tablewrapper package.

--- src/main/java/com/tawelib/groupfive/tablewrapper/CopiesTableWrapper.java
+++ src/main/java/com/tawelib/groupfive/tablewrapper/CopiesTableWrapper.java
@@ -8,25 +8,49 @@ import java.time.LocalDateTime;
 
 /**
  * This class wraps information about Resources that is shown in an FXML table.
+ *
+ * @author Petr Hoffmann
+ * @version 1.0
  */
 public class CopiesTableWrapper {
 
+  private Copy copy;
+  private LeaseRepository leaseRepository;
+
+  /**
+   * Constructs a new wrapper for a copy.
+   *
+   * @param copy Copy.
+   * @param leaseRepository Lease repository.
+   */
   public CopiesTableWrapper(Copy copy, LeaseRepository leaseRepository) {
     this.copy = copy;
     this.leaseRepository = leaseRepository;
   }
 
-  private Copy copy;
-  private LeaseRepository leaseRepository;
-
+  /**
+   * Returns the copy ID.
+   *
+   * @return Copy ID.
+   */
   public String getId() {
     return copy.getId();
   }
 
+  /**
+   * Returns the copy status.
+   *
+   * @return Copy status.
+   */
   public CopyStatus getStatus() {
     return copy.getStatus();
   }
 
+  /**
+   * Returns the resource title.
+   *
+   * @return Resource title.
+   */
   public String getTitle() {
     return copy.getResource().getTitle();
   }
@@ -46,6 +70,11 @@ public class CopiesTableWrapper {
     }
   }
 
+  /**
+   * Returns the wrapped item (Copy).
+   *
+   * @return The wrapped copy.
+   */
   public Copy getCopy() {
     return copy;
   }
--- src/main/java/com/tawelib/groupfive/tablewrapper/ResourceTableWrapper.java
+++ src/main/java/com/tawelib/groupfive/tablewrapper/ResourceTableWrapper.java
@@ -5,31 +5,64 @@ import com.tawelib.groupfive.entity.ResourceType;
 
 /**
  * This class wraps information about Resources that is shown in an FXML table.
+ *
+ * @author Petr Hoffmann
+ * @version 1.0
  */
 public class ResourceTableWrapper {
 
+  /**
+   * Constructs the resource wrapper.
+   *
+   * @param resource The resource to wrap around.
+   */
   public ResourceTableWrapper(Resource resource) {
     this.resource = resource;
   }
 
   private Resource resource;
 
+  /**
+   * Returns the resource ID.
+   *
+   * @return The resource ID.
+   */
   public String getId() {
     return resource.getResourceId();
   }
 
+  /**
+   * Returns the resource title.
+   *
+   * @return The resource title.
+   */
   public String getTitle() {
     return resource.getTitle();
   }
 
+  /**
+   * Returns resource the year.
+   *
+   * @return The resource year.
+   */
   public int getYear() {
     return resource.getYear();
   }
 
+  /**
+   * The resource type.
+   *
+   * @return The resource type.
+   */
   public ResourceType getType() {
     return resource.getType();
   }
 
+  /**
+   * Returns the resource.
+   *
+   * @return The resource.
+   */
   public Resource getResource() {
     return resource;
   }
