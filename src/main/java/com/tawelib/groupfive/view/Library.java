760db47b055084b1870667f091d561cefa94ac3b
hoffic-cz <petr.hoffic@gmail.com>
2018-12-07 12:08:56 +0000

    Showing startup errors in the GUI.

--- src/main/java/com/tawelib/groupfive/view/Library.java
+++ src/main/java/com/tawelib/groupfive/view/Library.java
@@ -2,12 +2,14 @@ package com.tawelib.groupfive.view;
 
 import com.tawelib.groupfive.entity.Customer;
 import com.tawelib.groupfive.entity.Librarian;
+import com.tawelib.groupfive.util.AlertHelper;
 import com.tawelib.groupfive.util.FileSystemHelper;
 import com.tawelib.groupfive.util.SceneHelper;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Date;
 import javafx.application.Application;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Library extends Application {
@@ -16,8 +18,15 @@ public class Library extends Application {
 
   private com.tawelib.groupfive.entity.Library library;
 
+  private static String announceText;
+
   public static void main(String[] args) {
     launch(args);
+  }
+
+  public static void launchAnnounce(String text) {
+    announceText = text;
+    launch();
   }
 
   @Override
@@ -37,6 +46,11 @@ public class Library extends Application {
     }
 
     primaryStage.show();
+
+    if (announceText != null) {
+      AlertHelper.alert(AlertType.ERROR, announceText);
+      System.exit(1);
+    }
   }
 
   @Override
