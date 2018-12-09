576249b24f22cb58ce45e4e56112b034692b5d8a
hoffic-cz <petr.hoffic@gmail.com>
2018-12-09 12:33:22 +0000

    Added Javadoc to exceptions.

--- src/main/java/com/tawelib/groupfive/exception/InvalidProductKeyException.java
+++ src/main/java/com/tawelib/groupfive/exception/InvalidProductKeyException.java
@@ -1,5 +1,11 @@
 package com.tawelib.groupfive.exception;
 
+/**
+ * This exception gets thrown when an invalid product key is being verified.
+ *
+ * @author Petr Hoffmann
+ * @version 1.0
+ */
 public class InvalidProductKeyException extends Exception {
 
 }
