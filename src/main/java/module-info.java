07ab3635f7fe4b27e4fb8c6447865ba3fa6a7906
hoffic-cz <petr.hoffic@gmail.com>
2018-12-09 12:28:07 +0000

    Fixed Javadoc generator arguments and module access.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,9 +8,14 @@ module com.tawelib.groupfive {
   requires javafx.media;
 
   opens com.tawelib.groupfive;
-  opens com.tawelib.groupfive.view;
   opens com.tawelib.groupfive.draw;
-  opens com.tawelib.groupfive.fxmlcontroller;
   opens com.tawelib.groupfive.entity;
+  opens com.tawelib.groupfive.exception;
+  opens com.tawelib.groupfive.fxmlcontroller;
+  opens com.tawelib.groupfive.manager;
+  opens com.tawelib.groupfive.repository;
   opens com.tawelib.groupfive.tablewrapper;
+  opens com.tawelib.groupfive.testdata;
+  opens com.tawelib.groupfive.util;
+  opens com.tawelib.groupfive.view;
 }
