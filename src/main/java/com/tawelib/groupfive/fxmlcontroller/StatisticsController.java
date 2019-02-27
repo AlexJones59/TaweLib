f29b4a5225e82f2cd2f860b354d8064b10ac7ad5
Shree Desai <shree.desai.2407@gmail.com>
2019-02-27 23:02:50 +0000

    Created the Statistics fxml and controller

---
+++ src/main/java/com/tawelib/groupfive/fxmlcontroller/StatisticsController.java
@@ -0,0 +1,12 @@
+package com.tawelib.groupfive.fxmlcontroller;
+
+/**
+ * The Statistics controller: A user can view info about borrowing statistics for themselves and an average user.
+ * They can also view the most popular resources over different time periods, with specifics given for each resource type.
+ * A Librarian will also be able to statistics pertaining to fines issued.
+ *
+ * @author Shree Desai
+ * @version 1.0
+ */
+public class StatisticsController extends BaseFxmlController {
+}
