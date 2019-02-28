13491b5dd08b1b1b426be24b5d8e3f0a6f53f69e
bobiandreev <bobiandreev9@gmail.com>
2019-02-28 13:57:46 +0000

    Added ResourceCap class and functionality and calle it where necessary.

--- src/main/java/com/tawelib/groupfive/fxmlcontroller/BorrowResourcesController.java
+++ src/main/java/com/tawelib/groupfive/fxmlcontroller/BorrowResourcesController.java
@@ -20,99 +20,98 @@ import javafx.scene.control.TextField;
 
 /**
  * This controls the Borrow Resource screen. This allows Librarians to loan resources to customers
- * that have above minimum
- * account balance. They loan a resource by entering its relevant copy ID.
+ * that have above minimum account balance. They loan a resource by entering its relevant copy ID.
  *
  * @author Shree Desai, Nayeem Mohammed
  * @version 1.0
  */
 public class BorrowResourcesController extends BaseFxmlController {
 
-    @FXML
-    private Button btnCancel;
+  @FXML
+  private Button btnCancel;
 
-    @FXML
-    private Label lblScreenTitle;
+  @FXML
+  private Label lblScreenTitle;
 
-    @FXML
-    private Button btnBorrow;
+  @FXML
+  private Button btnBorrow;
 
-    @FXML
-    private TextField txtResourceCopyId;
+  @FXML
+  private TextField txtResourceCopyId;
 
-    @FXML
-    private Label lblResourceId;
+  @FXML
+  private Label lblResourceId;
 
-    private Customer selectedCustomer = (Customer) BaseFxmlController.selectedUser;
+  private Customer selectedCustomer = (Customer) BaseFxmlController.selectedUser;
 
-    /**
-     * Checks whether user can borrow a resource based on availability or their
-     * account balance and then lets them borrow specified resource if allowed.
-     */
-    public void borrow() {
-        List<Lease> overdueLeases = library.getLeaseRepository()
-                .getCustomerOverdueLeases(selectedCustomer);
-        Copy requestedCopy = library.getCopyRepository()
-                .getSpecific(txtResourceCopyId.getText());
+  /**
+   * Checks whether user can borrow a resource based on availability or their account balance and
+   * then lets them borrow specified resource if allowed.
+   */
+  public void borrow() {
+    List<Lease> overdueLeases = library.getLeaseRepository()
+        .getCustomerOverdueLeases(selectedCustomer);
+    Copy requestedCopy = library.getCopyRepository()
+        .getSpecific(txtResourceCopyId.getText());
 
+    if (!ResourceCapManager
+        .isOverResourceCap(library, selectedCustomer, requestedCopy.getResource())) {
 
-        if (!ResourceCapManager.isOverResourceCap(library, selectedCustomer, requestedCopy.getResource())) {
-
-            if (requestedCopy == null) {
-                AlertHelper.alert(AlertType.ERROR, "Copy ID is not valid.");
-            } else {
-                if (selectedCustomer.getAccountBalance() < 0) {
-                    AlertHelper.alert(
-                            AlertType.ERROR,
-                            "Your balance is below the minimum."
-                    );
-                    back();
-                } else {
-                    if (!overdueLeases.isEmpty()) {
-                        AlertHelper.alert(
-                                AlertType.ERROR,
-                                "You have overdue copies."
-                        );
-                    } else {
-                        if (!requestedCopy.getStatus().equals(CopyStatus.AVAILABLE)) {
-                            AlertHelper.alert(
-                                    AlertType.ERROR,
-                                    "This copy is not available to borrow."
-                            );
-                            back();
-                        } else {
-                            CopyManager.borrowResourceCopy(
-                                    library,
-                                    txtResourceCopyId.getText(),
-                                    selectedCustomer.getUsername()
-                            );
-                            AlertHelper.alert(
-                                    AlertType.INFORMATION,
-                                    "Borrowed."
-                            );
-                            back();
-                        }
-                    }
-                }
-            }
+      if (requestedCopy == null) {
+        AlertHelper.alert(AlertType.ERROR, "Copy ID is not valid.");
+      } else {
+        if (selectedCustomer.getAccountBalance() < 0) {
+          AlertHelper.alert(
+              AlertType.ERROR,
+              "Your balance is below the minimum."
+          );
+          back();
         } else {
-            AlertHelper.alert(Alert.AlertType.ERROR, "You have exceeded the resource cap. " +
-                    "An item must be returned before another can be borrowed.");
+          if (!overdueLeases.isEmpty()) {
+            AlertHelper.alert(
+                AlertType.ERROR,
+                "You have overdue copies."
+            );
+          } else {
+            if (!requestedCopy.getStatus().equals(CopyStatus.AVAILABLE)) {
+              AlertHelper.alert(
+                  AlertType.ERROR,
+                  "This copy is not available to borrow."
+              );
+              back();
+            } else {
+              CopyManager.borrowResourceCopy(
+                  library,
+                  txtResourceCopyId.getText(),
+                  selectedCustomer.getUsername()
+              );
+              AlertHelper.alert(
+                  AlertType.INFORMATION,
+                  "Borrowed."
+              );
+              back();
+            }
+          }
         }
+      }
+    } else {
+      AlertHelper.alert(Alert.AlertType.ERROR, "You have exceeded the resource cap. " +
+          "An item must be returned before another can be borrowed.");
     }
+  }
 
-    /**
-     * Returns to the user information screen.
-     */
-    public void cancel() {
-        back();
-    }
+  /**
+   * Returns to the user information screen.
+   */
+  public void cancel() {
+    back();
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void back() {
-        SceneHelper.setUpScene(this, "UserInformation");
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void back() {
+    SceneHelper.setUpScene(this, "UserInformation");
+  }
 }
--- src/main/java/com/tawelib/groupfive/manager/RequestManager.java
+++ src/main/java/com/tawelib/groupfive/manager/RequestManager.java
@@ -13,52 +13,51 @@ import javafx.scene.control.Alert;
 import java.util.List;
 
 /**
- * File Name - RequestManager.java The Request Manager class handles data
- * flow between the Request Repository and the GUI interfaces.
+ * File Name - RequestManager.java The Request Manager class handles data flow between the Request
+ * Repository and the GUI interfaces.
  *
  * @author Shree Desai
  * @version 1.0
  */
 public class RequestManager {
 
-    /**
-     * Create request for a resource while also setting due date of oldest
-     * borrowed copy.
-     *
-     * @param library           the library
-     * @param customer          the customer
-     * @param requestedResource the requested resource
-     */
-    public static void createRequest(Library library, Customer customer,
-                                     Resource requestedResource) {
+  /**
+   * Create request for a resource while also setting due date of oldest borrowed copy.
+   *
+   * @param library the library
+   * @param customer the customer
+   * @param requestedResource the requested resource
+   */
+  public static void createRequest(Library library, Customer customer,
+      Resource requestedResource) {
 
-        if (!ResourceCapManager.isOverResourceCap(library, customer, requestedResource)) {
+    if (!ResourceCapManager.isOverResourceCap(library, customer, requestedResource)) {
 
-            Request newRequest = new Request(customer, requestedResource);
-            library.getRequestRepository().add(newRequest);
-            library.getLeaseRepository().getCustomerCurrentLeases(customer);
-            //Sets Due Date of oldest borrowed copy.
-            List<Copy> resourceCopies = library.getCopyRepository()
-                    .getResourceCopies(requestedResource);
-            Copy oldestCopy = resourceCopies.get(0);
-            for (Copy copy : resourceCopies) {
-                if (library.getLeaseRepository().getCopyCurrentLease(copy).getDateLeased()
-                        .isBefore(library.getLeaseRepository().getCopyCurrentLease(oldestCopy)
-                                .getDateLeased())
-                        && library.getLeaseRepository().getCopyCurrentLease(copy).getDueDate()
-                        != null) {
-                    oldestCopy = copy;
+      Request newRequest = new Request(customer, requestedResource);
+      library.getRequestRepository().add(newRequest);
+      library.getLeaseRepository().getCustomerCurrentLeases(customer);
+      //Sets Due Date of oldest borrowed copy.
+      List<Copy> resourceCopies = library.getCopyRepository()
+          .getResourceCopies(requestedResource);
+      Copy oldestCopy = resourceCopies.get(0);
+      for (Copy copy : resourceCopies) {
+        if (library.getLeaseRepository().getCopyCurrentLease(copy).getDateLeased()
+            .isBefore(library.getLeaseRepository().getCopyCurrentLease(oldestCopy)
+                .getDateLeased())
+            && library.getLeaseRepository().getCopyCurrentLease(copy).getDueDate()
+            != null) {
+          oldestCopy = copy;
 
-                }
-            }
-
-            CopyManager.generateDueDate(
-                    library.getLeaseRepository().getCopyCurrentLease(oldestCopy));
-        } else {
-            System.out.println("You have exceeded the resource cap. " +
-                    "An item must be returned before another can be borrowed.");
-            AlertHelper.alert(Alert.AlertType.ERROR, "You have exceeded the resource cap. " +
-                    "An item must be returned before another can be borrowed.");
         }
+      }
+
+      CopyManager.generateDueDate(
+          library.getLeaseRepository().getCopyCurrentLease(oldestCopy));
+    } else {
+      System.out.println("You have exceeded the resource cap. " +
+          "An item must be returned before another can be borrowed.");
+      AlertHelper.alert(Alert.AlertType.ERROR, "You have exceeded the resource cap. " +
+          "An item must be returned before another can be borrowed.");
     }
+  }
 }
