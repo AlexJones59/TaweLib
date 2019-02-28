e4be81e20f943f6b213a9022bd45fbc34bdc8bbd
AJones59 <xanjones59@gmail.com>
2019-02-28 11:09:34 +0000

    -Added Rating repository class

---
+++ src/main/java/com/tawelib/groupfive/repository/RatingRepository.java
@@ -0,0 +1,60 @@
+package com.tawelib.groupfive.repository;
+
+import com.tawelib.groupfive.entity.Rating;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * RatingRepository.java The rating repository class that handles all ratings and reviews
+ *
+ * @author Alex Jones
+ * @version 1.0
+ */
+public class RatingRepository implements BaseRepository<Rating> {
+
+  private ArrayList<Rating> ratings;
+
+  /**
+   * Instantiates a new RatingRepository.
+   */
+  public RatingRepository() {
+    ratings = new ArrayList<>();
+  }
+
+  /**
+   * Returns all ratings for a particular resource.
+   *
+   * @param resource resource being searched for
+   * @return List of all ratings for resource
+   */
+  public List<Rating> getResourcesRatings(String resource) {
+    ArrayList<Rating> results = new ArrayList<>();
+
+    for (Rating rating : ratings) {
+      if (rating.getRatedResource().equals(resource)) {
+        results.add(rating);
+      }
+    }
+
+    return results;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public List<Rating> getAll() {
+    return ratings;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void add(Rating rating) {
+    if (!ratings.contains(rating)) {
+      ratings.add(rating);
+    }
+  }
+}
