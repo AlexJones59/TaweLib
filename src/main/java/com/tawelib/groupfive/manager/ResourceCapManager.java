65137b425cf7be93d52a71b34629bef9c9804652
bobiandreev <bobiandreev9@gmail.com>
2019-02-28 13:57:46 +0000

    Added ResourceCap class and functionality and calle it where necessary.

--- src/main/java/com/tawelib/groupfive/manager/ResourceCapManager.java
+++ src/main/java/com/tawelib/groupfive/manager/ResourceCapManager.java
@@ -2,28 +2,37 @@ package com.tawelib.groupfive.manager;
 
 import com.tawelib.groupfive.entity.*;
 
+/**
+ * This is the resource cap method. It checks how many items has the user already borrowed and
+ * returns false if the number + the resource they are trying to borrow is under the cap and true
+ * otherwise.
+ *
+ * @author Boris Andreev
+ * @version 1.0
+ */
+
 public class ResourceCapManager {
 
-    private static final int cap = 5;
+  private static final int cap = 5;
 
-    public static boolean isOverResourceCap(Library library, Customer customer, Resource resource) {
-        int borrowedItems = 0;
-        for (Lease lease : library.getLeaseRepository().getCustomerCurrentLeases(customer)) {
-            if (lease.getBorrowedCopy().getResource() instanceof Laptop) {
-                borrowedItems += 3;
-            } else {
-                borrowedItems++;
-            }
-        }
-        if (resource instanceof Laptop) {
-            borrowedItems += 3;
-        } else {
-            borrowedItems += 1;
-        }
-        if (borrowedItems <= cap) {
-            return false;
-        } else {
-            return true;
-        }
+  public static boolean isOverResourceCap(Library library, Customer customer, Resource resource) {
+    int borrowedItems = 0;
+    for (Lease lease : library.getLeaseRepository().getCustomerCurrentLeases(customer)) {
+      if (lease.getBorrowedCopy().getResource() instanceof Laptop) {
+        borrowedItems += 3;
+      } else {
+        borrowedItems++;
+      }
     }
+    if (resource instanceof Laptop) {
+      borrowedItems += 3;
+    } else {
+      borrowedItems += 1;
+    }
+    if (borrowedItems <= cap) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
