b9b76b45018c1a32063ea41637a11fe70f1b8fd2
Rimantas Kazlauskas <konulv731@gmail.com>
2019-03-03 19:14:06 +0000

    stopped java complaining about repositories

--- src/main/java/com/tawelib/groupfive/repository/EventRepository.java
+++ src/main/java/com/tawelib/groupfive/repository/EventRepository.java
@@ -2,6 +2,34 @@ package com.tawelib.groupfive.repository;
 
 import com.tawelib.groupfive.entity.Event;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventRepository implements BaseRepository<Event> {
 
+    private ArrayList<Event> events;
+
+    public EventRepository() {
+        events = new ArrayList<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Event> getAll() {
+        return events;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void add(Event event) {
+        if (!events.contains(event)) {
+            events.add(event);
+        }
+    }
+
 }
+
+
--- src/main/java/com/tawelib/groupfive/repository/ParticipationRepository.java
+++ src/main/java/com/tawelib/groupfive/repository/ParticipationRepository.java
@@ -2,6 +2,31 @@ package com.tawelib.groupfive.repository;
 
 import com.tawelib.groupfive.entity.Participation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParticipationRepository implements BaseRepository<Participation> {
+    private ArrayList<Participation> participation;
+
+    public ParticipationRepository(){
+        participation = new ArrayList<>();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Participation> getAll() {
+        return participation;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void add(Participation participation) {
+        if (!this.participation.contains(participation)) {
+            this.participation.add(participation);
+        }
+    }
 
 }
