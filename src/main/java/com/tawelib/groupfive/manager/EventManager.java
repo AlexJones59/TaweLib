16b6769113d637b17a98b7690bb3be0c97d295c6
Rimantas Kazlauskas <konulv731@gmail.com>
2019-03-04 13:40:12 +0000

    updated Manager

--- src/main/java/com/tawelib/groupfive/manager/EventManager.java
+++ src/main/java/com/tawelib/groupfive/manager/EventManager.java
@@ -1,25 +1,29 @@
 package com.tawelib.groupfive.manager;
 import com.tawelib.groupfive.entity.User;
 import com.tawelib.groupfive.entity.Event;
+import com.tawelib.groupfive.entity.Participation;
+import com.tawelib.groupfive.entity.Library;
 
 import java.util.ArrayList;
 
 public class EventManager {
 
-    public static void addEvent(Event event){
+    public static void addEvent(Library library, Event event){
+        library.getEventRepository().add(event);
+    }
+
+    public static void joinEvent(Library library, User user, Event event){
+        Participation participation = new Participation(user.getUsername(), event.getEventId());
+        library.getParticipationRepository().add(participation);
+    }
+    public static boolean hasJoined(Library library, User user, Event event){
 
     }
 
-    public static void joinEvent(User user, Event event){
-
-    }
-    public static boolean hasJoined(User user, Event event){
-
-    }
-    public static ArrayList<Event> getUpcomingEvents(){
-
-    }
-    private boolean checkCap(String eventID){
+    private boolean eventFull(Library library, Event event){
+        
+        int participating = library.getParticipationRepository().getNumberOfParticipants(;
+        return participating == event.getCapacity();
 
     }
 
